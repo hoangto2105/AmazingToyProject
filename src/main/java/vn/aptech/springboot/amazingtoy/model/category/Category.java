04495a34816d26ab2478089b2e2101a3b493b414
Hoang To <hoangto@Hoangs-MacBook-Pro-2.local>
2020-10-29 12:36:51 +0700

    Completed Category

---
+++ src/main/java/vn/aptech/springboot/amazingtoy/model/category/Category.java
@@ -0,0 +1,118 @@
+package vn.aptech.springboot.amazingtoy.model.category;
+
+import vn.aptech.springboot.amazingtoy.model.products.Products;
+import vn.aptech.springboot.amazingtoy.model.subcategory.Subcategory;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+
+@Entity
+@Table(name = "Category")
+@XmlRootElement
+@NamedQueries({
+        @NamedQuery(name = "Category.findAll", query = "SELECT c FROM Category c"),
+        @NamedQuery(name = "Category.findByCategoryID", query = "SELECT c FROM Category c WHERE c.categoryID = :categoryID"),
+        @NamedQuery(name = "Category.findByName", query = "SELECT c FROM Category c WHERE c.name = :name")})
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "category_ID")
+    private Integer categoryID;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "categoryId")
+    private Collection<Products> productsCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "catId")
+    private Collection<Subcategory> subcategoryCollection;
+
+    public Category() {
+    }
+
+    public Category(Integer categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public Category(Integer categoryID, String name) {
+        this.categoryID = categoryID;
+        this.name = name;
+    }
+
+    public Integer getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Integer categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlTransient
+    public Collection<Products> getProductsCollection() {
+        return productsCollection;
+    }
+
+    public void setProductsCollection(Collection<Products> productsCollection) {
+        this.productsCollection = productsCollection;
+    }
+
+    @XmlTransient
+    public Collection<Subcategory> getSubcategoryCollection() {
+        return subcategoryCollection;
+    }
+
+    public void setSubcategoryCollection(Collection<Subcategory> subcategoryCollection) {
+        this.subcategoryCollection = subcategoryCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (categoryID != null ? categoryID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Category)) {
+            return false;
+        }
+        Category other = (Category) object;
+        if ((this.categoryID == null && other.categoryID != null) || (this.categoryID != null && !this.categoryID.equals(other.categoryID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "vn.aptech.springboot.amazingtoy.model.category.Category[ categoryID=" + categoryID + " ]";
+    }
+
+}
+
